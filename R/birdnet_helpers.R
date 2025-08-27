1521059852ff4674c4d6eb7aa5ccdcf0b40c1a9a
Kelly Faller <107068971+kellyfaller@users.noreply.github.com>
2025-08-27 16:09:05 -0400

    Update birdnet_helpers.R

--- R/birdnet_helpers.R
+++ R/birdnet_helpers.R
@@ -1,1 +1,10 @@
+read_birdnet_predictions <- function(path) {
+  df <- vroom::vroom(path, show_col_types = FALSE)
+  assert_cols(df, c("site","dt_start","wav_path","segment_start_s","species","logit","confidence"), "BirdNET predictions")
+  df
+}
+
+pr_curve <- function(scores, labels) {
+  yardstick::pr_auc_vec(factor(labels, levels=c(0,1)), scores)
+}
 

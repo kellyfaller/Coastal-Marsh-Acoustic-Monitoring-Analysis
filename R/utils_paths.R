666f4a7639575a972c1b07e8812516f784c3fedc
Kelly Faller <107068971+kellyfaller@users.noreply.github.com>
2025-08-27 16:07:47 -0400

    Update utils_paths.R

--- R/utils_paths.R
+++ R/utils_paths.R
@@ -1,1 +1,8 @@
+fp <- function(...) normalizePath(file.path(...), mustWork = FALSE)
 
+load_config <- function(path = "configs/project.yml") {
+  yml <- yaml::read_yaml(path)
+  if (is.null(yml$paths) || is.null(yml$outputs) || is.null(yml$params))
+    cli::cli_abort("Config file missing required top-level keys: paths/outputs/params.")
+  yml
+}
